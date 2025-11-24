4d84eb9e1798d97d19636d5f03fa5401ce329341
MrSeager <seager1993@gmail.com>
2025-11-24 10:54:28 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,30 @@ import SectionSix from "@/components/SectionSix";
 import SectionSeven from "@/components/SectionSeven";
 import SectionEight from "@/components/SectionEight";
 
+export const metadata = {
+  title: "Qrates Why Page Exercise",
+  openGraph: {
+    title: "Qrates Why Page Exercise",
+    description: "Qrates Why Page Exercise. For creating this page Next.JS, TypeScript and Tailwind were used.",
+    images: [
+      {
+        url: "https://raw.githubusercontent.com/MrSeager/mr-seager-portfolio/refs/heads/main/src/images/075.jpg",
+        width: 900,
+        height: 660,
+        alt: "Meet the Team Section Preview",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Qrates Why Page Exercise",
+    description: "Qrates Why Page Exercise. For creating this page Next.JS, TypeScript and Tailwind were used.",
+    images: [
+      "https://raw.githubusercontent.com/MrSeager/mr-seager-portfolio/refs/heads/main/src/images/075.jpg",
+    ],
+  },
+}
+
 export default function Home() {
   return (
     <div className="overflow-hidden flex flex-col min-h-screen items-center justify-top bg-white">
